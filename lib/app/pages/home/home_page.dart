8fcd541cb462169acf7009b8148c1f6cfbfe6ac2
Gabriel Marques <gabrielmarksf@icloud.com>
2023-02-14 15:53:21 -0300

    feat: added home page and helpers

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,14 +1,49 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/loader.dart';
 import 'package:dw9_delivery_app/app/core/widgets/delivery_appbar.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(Duration(seconds: 2));
+          hideLoader();
+        },
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                    id: 0,
+                    name: 'Lanche X',
+                    description: 'DESCRICAO',
+                    image:
+                        'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                    price: 15.0,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
